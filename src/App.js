0fe15f0396296f26cf05292ceb1cb5dfcb76d336
OttoCarius <ottocarius1993@gmail.com>
2023-02-19 12:59:52 +0200

    fx

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
 import "./styles/main.css";
 
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/navbar/Navbar"
+import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Project from "./pages/Project";
 import Contacts from "./pages/Contacts";
 
-import ScrollToTop from "./utils/scrollToTop"
+import ScrollToTop from "./utils/scrollToTop";
 
 function App() {
   return (
-		<div className="App">
-			<Router>
-				<ScrollToTop />
-				<Navbar />
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/projects" element={<Projects />} />
-					<Route path="/project/:id" element={<Project />} />
-					<Route path="/contacts" element={<Contacts />} />
-					<Route path="*" element={<Home />} />
-				</Routes>
-				<Footer />
-			</Router>
-		</div>
+    <div className="App">
+      <ScrollToTop />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/project/:id" element={<Project />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
